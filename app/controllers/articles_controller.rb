2974f783c052916233f11fd88039609fb148069d
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-25 18:44:10 +0900

    Set up for update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show edit update]
+
   def index
+    # List all articles, with more recent ones at the top
     @articles = Article.order(created_at: :desc)
   end
 
@@ -9,26 +12,26 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.save
-      redirect_to article_path(@article)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @article.save
+    redirect_to article_path(@article)
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def show; end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
-
-  # def edit
-  # end
-
-  # def update
-  # end
 
   # def destroy
   # end
   private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
