11316982778420c40125e3820ae89964f4c44029
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-25 17:31:58 +0900

    Set up for Create and modified Index to list from the most recent one

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,24 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
   end
 
   def new
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-
-  # def create
-  # end
 
   # def edit
   # end
@@ -22,4 +28,9 @@ class ArticlesController < ApplicationController
 
   # def destroy
   # end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
